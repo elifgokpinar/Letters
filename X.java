9c91f05ade6bc4b2a6f713319b8ebc604fd43cfd
Elif Gökpınar <72789565+elifgokpinar@users.noreply.github.com>
2021-02-15 15:33:04 +0300

    Create X.java

---
+++ X.java
@@ -0,0 +1,21 @@
+//Elif Gökpınar - Grade 1 
+public class X{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  
+	int size1 = 13;	
+	for (int i = 0 ; i<size1 ; i++) {
+	  for(int j =0; j<size1 ; j++) {
+			if(i==j || j== size1 - i-1) 
+				System.out.print("*");
+				
+			else 
+			System.out.print(" ");	
+	  }
+	System.out.println();		
+  }
+  
+  }
+	}
+
